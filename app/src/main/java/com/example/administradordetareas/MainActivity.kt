9a09332f824c4247f508888d4a694bd2a995be6a
Manuel Alejandro Muñoz Zeballos <manuel.munoz3@unmsm.edu.pe>
2023-08-23 21:28:48 -0500

    Commit Administrador de Tareas

---
+++ app/src/main/java/com/example/administradordetareas/MainActivity.kt
@@ -0,0 +1,78 @@
+package com.example.administradordetareas
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.administradordetareas.ui.theme.AdministradorDeTareasTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            AdministradorDeTareasTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    TaskAdministrator()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun TaskAdministrator(modifier: Modifier = Modifier) {
+    val imagePainter = painterResource(R.drawable.ic_task_completed)
+
+    Column (
+        modifier = Modifier
+            .fillMaxWidth()
+            .fillMaxHeight(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ){
+        Image(painter = imagePainter, contentDescription = null)
+        Text(
+            text = stringResource(R.string.all_tasks_completed),
+            modifier = modifier
+                .padding(top = 24.dp, bottom = 8.dp),
+            fontWeight = FontWeight.Bold
+        )
+        Text(
+            text = stringResource(R.string.nice_work),
+            fontSize = 16.sp
+        )
+    }
+
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    AdministradorDeTareasTheme {
+        TaskAdministrator()
+    }
+}
